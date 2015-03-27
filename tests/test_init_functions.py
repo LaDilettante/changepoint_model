e86f75d35cd9104294f36306cc1d2feb57e1784c
Anh Le <aql3@duke.edu>
2015-03-27 02:41:47 -0400

    add first test

--- tests/test_init_functions.py
+++ tests/test_init_functions.py
@@ -1,6 +1,6 @@
-from changepoint import init_functions
+from changepoint.init_functions import *
 import pytest
 import numpy as np
 
-def test1():
-    assert(P(2) == np.array([[0.5, 0.5], [1]]))
+def test_P():
+    assert((P(2) == np.array([[0.5, 0.5], [1]])).all)
